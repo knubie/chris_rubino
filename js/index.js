aacaf73a577b8810c69e42aedb9ac23fd8088fee
Matt <matt@tankjetstudios.com>
2012-04-07 16:12:39 -0400

    added filters

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
+
+	// Visual effects
 	$('.work-item').hover(function(){
 		$(this).children('.desc').css('background-color', $(this).attr('data-color'));
 	},
 	function(){
 		$(this).children('.desc').css('background-color', '#FFFFFF');
 	});
+
+	// Filters
+	$('#filter > a').click(function(){
+		var cat = '.' + $(this).attr('data-cat');
+		if (cat === '.all') {
+			$('.work-item').removeClass('hide-me').show();
+		} else {
+			$('.work-item').addClass('hide-me');
+			$(cat).removeClass('hide-me');
+			$('.hide-me').hide();
+			$(cat).show();
+		};
+
+	});
+
 });
